4dd59a83cf1d5ea64320296a8b621e873bd01f7b
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-04-07 22:38:45 +0300

    Initial Commit

---
+++ ios-cryptocurrency/ios_cryptocurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cryptocurrencyApp.swift
+//  ios-cryptocurrency
+//
+//  Created by Oleksandr Rolik on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_cryptocurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-cryptocurrencyTests/ios_cryptocurrencyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cryptocurrencyTests.swift
+//  ios-cryptocurrencyTests
+//
+//  Created by Oleksandr Rolik on 07.04.2025.
+//
+
+import Testing
+@testable import ios_cryptocurrency
+
+struct ios_cryptocurrencyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
