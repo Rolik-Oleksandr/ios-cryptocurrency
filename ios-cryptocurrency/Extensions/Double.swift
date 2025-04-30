426588fb6a201d5aa8915497654cc74c9966fab2
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-01 00:02:32 +0300

    extend Double for convert currency

---
+++ ios-cryptocurrency/Extensions/Double.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+extension Double {
+    
+    ///Convert a Double into a currency with 2-6 decimal places
+    ///```
+    ///1234.56 -> $1,234.56
+    ///12.3456 -> $12.3456
+    ///0.123456 -> $0.123456
+    ///```
+    
+    private var currencyFormatter: NumberFormatter {
+        let formater = NumberFormatter()
+        formater.usesGroupingSeparator = true
+        formater.numberStyle = .currency
+        formater.locale = .current
+        formater.currencyCode = "USD"
+        formater.currencySymbol = "$"
+        formater.minimumFractionDigits = 2
+        formater.maximumFractionDigits = 6
+        return formater
+    }
+    
+    ///Convert a Double into a currency as a String with 2-6 decimal places
+    ///```
+    ///1234.56 -> "$1,234.56"
+    ///12.3456 -> "$12.3456"
+    ///0.123456 -> "$0.123456"
+    ///`
+    
+    func asCurrencyWith6Decimals()-> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter.string(from: number) ?? "$0.00"
+    }
+    
+}
