fca01901a9b76c475599f65ebe660763e29e552c
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-02 23:54:41 +0300

    edit api request coin model

--- ios-cryptocurrency/Model/CoinModel.swift
+++ ios-cryptocurrency/Model/CoinModel.swift
@@ -1,50 +1,60 @@
 import Foundation
 
 struct CoinModel: Identifiable, Codable {
+    let currentHoldings: Double?
+    
     let id, symbol, name: String
     let image: String
     let currentPrice: Double
-    let marketCap, marketCapRank, fullyDilutedValuation: Double?
-    let totalVolume, high24H, low24H: Double?
-    let priceChange24H, priceChangePercentage24H: Double?
+    let marketCap: Double?
+    let marketCapRank: Int?
+    let fullyDilutedValuation: Double?
+    let totalVolume: Double?
+    let high24H: Double?
+    let low24H: Double?
+    let priceChange24H: Double?
+    let priceChangePercentage24H: Double?
     let marketCapChange24H: Double?
     let marketCapChangePercentage24H: Double?
-    let circulatingSupply, totalSupply, maxSupply, ath: Double?
+    let circulatingSupply: Double?
+    let totalSupply: Double?
+    let maxSupply: Double?
+    let ath: Double?
     let athChangePercentage: Double?
     let athDate: String?
-    let atl, atlChangePercentage: Double?
+    let atl: Double?
+    let atlChangePercentage: Double?
     let atlDate: String?
     let lastUpdated: String?
-    let currentHoldings: Double?
     
     enum CodingKeys: String, CodingKey {
         case id, symbol, name, image
-        case currentPrice = "currentPrice"
-        case marketCap = "marketCap"
-        case marketCapRank = "marketCapRank"
-        case fullyDilutedValuation = "fullyDilutedValuation"
-        case totalVolume = "totalVolume"
-        case high24H = "high24h"
-        case low24H = "low24h"
-        case priceChange24H = "priceChange24h"
-        case marketCapChange24H = "marketCapChange24h"
-        case priceChangePercentage24H = "priceChangePercentage24h"
-        case marketCapChangePercentage24H = "marketCapChangePercentage24h"
-        case circulatingSupply = "circulatingSupply"
-        case totalSupply = "totalSupply"
-        case maxSupply = "maxSupply"
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case marketCapRank = "market_cap_rank"
+        case fullyDilutedValuation = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+        case marketCapChange24H = "market_cap_change_24h"
+        case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+        case circulatingSupply = "circulating_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
         case ath = "ath"
-        case athChangePercentage = "athChangePercentage"
-        case athDate = "athDate"
+        case athChangePercentage = "ath_change_percentage"
+        case athDate = "ath_date"
         case atl = "atl"
-        case atlChangePercentage = "atlChangePercentage"
-        case atlDate = "atlDate"
-        case lastUpdated = "lastUpdated"
+        case atlChangePercentage = "atl_change_percentage"
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
         case currentHoldings = "currentHoldings"
     }
     
     func updateHoldings(amount: Double) -> CoinModel {
-        return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, currentHoldings: amount)
+        return CoinModel(currentHoldings: amount, id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated)
     }
     
     var currentHoldingsValue: Double {
