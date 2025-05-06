89b4f294b9a63b688d3ec5454752e8af3957fe32
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-07 00:01:45 +0300

    implement saving images of coins to local file manager

--- ios-cryptocurrency/Service/CoinImageService.swift
+++ ios-cryptocurrency/Service/CoinImageService.swift
@@ -7,13 +7,27 @@ class CoinImageService {
     
     private var ImageSubscription: AnyCancellable?
     private var coin: CoinModel
+    private var fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+//            print("Get image from local")
+        } else {
+            downloadCoinImage()
+//            print("Download image")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         ImageSubscription = NetworkingManager.download(url: url)
@@ -21,8 +35,10 @@ class CoinImageService {
                 return UIImage(data: data)!
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.ImageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.ImageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
