85c082b4b6c0cfac9ebf7a21225be92045953722
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-10 21:32:00 +0300

    implement get statistic from API and create statistic model

--- ios-cryptocurrency/ViewModel/StartViewModel.swift
+++ ios-cryptocurrency/ViewModel/StartViewModel.swift
@@ -3,11 +3,13 @@ import Combine
 
 class StartViewModel: ObservableObject {
     
+    @Published var statistic: [StatisticModel] = []
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private var marketDataService = MarketDataService()
     private var cansellables = Set<AnyCancellable>()
     
     init() {
@@ -16,10 +18,10 @@ class StartViewModel: ObservableObject {
     
     func addSubscribers() {
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5) , scheduler: DispatchQueue.main)
             .map { (text, startingCoins) -> [CoinModel] in
-                 
+                
                 guard !text.isEmpty else {
                     return startingCoins
                 }
@@ -35,5 +37,33 @@ class StartViewModel: ObservableObject {
                 self?.allCoins = returnedCoins
             }
             .store(in: &cansellables )
+        
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnedStatistics) in
+                self?.statistic = returnedStatistics
+            }
+            .store(in: &cansellables)
+    }
+    
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel] {
+        var statistics: [StatisticModel] = []
+        guard let data = marketDataModel else {
+            return statistics
+        }
+        
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange:
+                                        data.marketCapChangePercentage24HUsd)
+        let volume = StatisticModel(title: "Volume", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+        
+        statistics.append(contentsOf: [
+            marketCap,
+            volume,
+            btcDominance,
+            portfolio
+        ])
+        return statistics
     }
 }
